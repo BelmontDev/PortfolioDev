970e507c455f3f0442336579b182876e8aa98593
Gilbert Lomeli <gilbertlomlloa@gmail.com>
2024-07-25 13:37:19 -0700

    Update New Resume

--- JS/script.js
+++ JS/script.js
@@ -26,7 +26,7 @@ function copiarCorreo() {
 
 function abrirPDF() {
   // Ruta al archivo PDF
-  var pdfURL = "JS/ResumeGLU.pdf";
+  var pdfURL = "JS/ResumeGiLU.pdf";
   
   // Abre el PDF en una nueva pestaña
   window.open(pdfURL, "_blank");
